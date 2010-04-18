e39f274b6b764188a433757b3078971b8467d2e1
kirk megna <kirk.tke@gmail.com>
2010-04-18 04:03:59 -0400

    Created cashflow and payrate functions. Established relationship between user, bought_stock, and stock.

--- app/models/bought_stock.rb
+++ app/models/bought_stock.rb
@@ -2,4 +2,9 @@ class BoughtStock < ActiveRecord::Base
   #DO VALIDATIONS KIRK!!
   belongs_to :user
   belongs_to :stock
+  
+  def current_value
+    stock.value*quantity
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  
   has_many :real_estates
   has_many :bought_stocks
   has_many :stocks, :through => :bought_stocks
   JOB_TYPES = %w(high middle low)
+  
+  PAY_RATES = {"high" => 35, "middle" => 22, "low" => 10}
   
   validates_inclusion_of :job, :in => JOB_TYPES
 
@@ -10,17 +13,21 @@ class User < ActiveRecord::Base
   validates_inclusion_of :job_hours, :in => 0..10, :if => :is_middle_class_job?
   validates_inclusion_of :job_hours, :in => 0..8, :if => :is_lower_class_job?
   
-  validates_format_of :name, :with => /^[0-9a-z _-\.]{3,10}$/i
+  validates_format_of :name, :with => /^[0-9a-z_ \-\.]{3,10}$/i
   
   validates_format_of :login, :with => /^[0-9a-z]{3,15}$/i
   validates_uniqueness_of :login
   
   validates_format_of :password, :with => /^[0-9a-z]{6,20}$/i
   
+  def payrate
+    PAY_RATES[job]
+  end
+  
   def is_upper_class_job?
     job=="high"
   end
-  
+   
   def is_middle_class_job?
     job=="middle"
   end
@@ -28,6 +35,14 @@ class User < ActiveRecord::Base
   def is_lower_class_job?
     job=="low"
   end
-  
+ 
+  def cashflow
+    job_hours*payrate
+  end
+ # def a cashflow function (kirk.cashflow) to give me the total cashflow from RE and Job
+ 
+  def net_stock_value
+    bought_stocks.inject(0) {|value,x| value + x.current_value}
+  end
   
 end
